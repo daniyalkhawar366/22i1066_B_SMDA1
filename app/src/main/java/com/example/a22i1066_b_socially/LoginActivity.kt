7c67602be4aa58972c435a5596834d4ba306fb26
daniyalkhawar366 <daniyalkhawar3@gmail.com>
2025-09-14 01:19:15 +0500

    minor frontend changes

---
+++ app/src/main/java/com/example/a22i1066_b_socially/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a22i1066_b_socially
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // You'll need to create a login layout file
+    }
+}
