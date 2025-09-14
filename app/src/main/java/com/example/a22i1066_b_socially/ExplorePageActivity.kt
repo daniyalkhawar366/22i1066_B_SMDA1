af7b17af65f789554b6745641e105a90ea2a8f9a
daniyalkhawar366 <daniyalkhawar3@gmail.com>
2025-09-14 12:17:24 +0500

    explore page connected

---
+++ app/src/main/java/com/example/a22i1066_b_socially/ExplorePageActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a22i1066_b_socially
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class ExplorePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.explore)
+
+    }
+}
--- app/src/main/java/com/example/a22i1066_b_socially/FYPActivity.kt
+++ app/src/main/java/com/example/a22i1066_b_socially/FYPActivity.kt
@@ -1,12 +1,21 @@
 package com.example.a22i1066_b_socially
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class FYPActivity : AppCompatActivity() {
+    private lateinit var explorepg: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.foryou)
+        explorepg = findViewById(R.id.explorepg)
+        explorepg.setOnClickListener {
+            val intent = Intent(this, ExplorePageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
